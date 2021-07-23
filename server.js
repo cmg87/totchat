a61bf47e78c5bcd446bd4b60b63a181f68bd59d1
chris <chrisgoerler@gmail.com>
2021-07-22 21:05:20 -0400

    serve static files

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
