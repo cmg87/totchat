067eddaccc76c118fb35d6184e0330b797b1230a
chris <chrisgoerler@gmail.com>
2021-07-22 20:38:16 -0400

    heroku try 3

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static("client/build"));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
